a459b0b9fb7b0215b1bab26ee40b8733f1050757
acunningham3 <43209704+acunningham3@users.noreply.github.com>
2018-11-04 22:18:32 -0500

    Update ping.py

--- ping.py
+++ ping.py
@@ -4,18 +4,19 @@ import os
 import time
 import socket
 import urllib2
+import sys
 
-hostname = "eng-svr-1"
-port = 8877
+hostname = sys.argv[1]
+port = sys.argv[2]
 
 def internet_on():
     try:
-        urllib2.urlopen('http://eng-svr-1:8877', timeout=1)
-	print hostname, 'is up!'
+        urllib2.urlopen(hostname + ':' + port, timeout=1)
+	print hostname + ':' + port, 'is up!'
 	time.sleep(2)
         return True
     except urllib2.URLError as err: 
-	print hostname, 'is down!'
+	print hostname + ':' + port, 'is down!'
 	time.sleep(2)
         return False
 
